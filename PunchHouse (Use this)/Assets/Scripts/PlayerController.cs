49f83475b0cf39bd5c1c92dd6e72300b1b7c0faa
amaki21 <makia0060@my.uwstout.edu>
2016-02-03 14:00:23 -0600

    Working Player Movement, New Version
    Use the folder that says "Use this"

---
+++ PunchHouse (Use this)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpHeight;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded; //true on ground, false in air
+    
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+
+	// Update is called once per frame
+	void Update () 
+    {
+	
+        if(Input.GetKeyDown (KeyCode.Space) && grounded)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);//gets value of character that he already has while moving sideways
+        }
+
+        if (Input.GetKey (KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);//gets value of character that he already has while moving up or down
+        }
+
+        if (Input.GetKey (KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+	}
+}
