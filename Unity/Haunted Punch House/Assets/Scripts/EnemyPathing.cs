3e5c02cb0a6f7733d50194a47990caf837778849
Scoopadoop <giesfeldte0427@uwstout.edu>
2016-02-07 17:57:16 -0600

    Jangles for ghosts

--- Unity/Haunted Punch House/Assets/Scripts/EnemyPathing.cs
+++ Unity/Haunted Punch House/Assets/Scripts/EnemyPathing.cs
@@ -28,11 +28,11 @@ public class EnemyPathing : MonoBehaviour
 
         if (moveSpeed > 0)
         {
-            transform.localScale = new Vector3(0.5f, 0.5f, 1f);
+            transform.localScale = new Vector3(1.0f, 1.0f, 1f);
         }
         else if (moveSpeed < 0)
         {
-            transform.localScale = new Vector3(-0.5f, 0.5f, 1f);
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1f);
         }
     }
 
